d4cef5d62ba03cf73b8493e89d82a5a820232ee5
faaip <frederikjuutilainen@gmail.com>
2017-08-27 23:18:08 +0200

    fix preload

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var backgroundVideo;
-var soundPlayer;
-var borderPadding = 0;
+var background_img;
+var isVideoLoaded = false;
 
 // Text stuff
-var font
+var font;
 var opacity = 0;
 var fadingIn = true;
 var textIndex = 0;
@@ -17,16 +17,34 @@ var textStrings = ["First, tell me what you're struggling with.", "(Be specific,
 
 function preload() {
 
-    // load soundcloud player
-    var iframe = document.createElement('iframe');
-    var start_track = Math.floor((Math.random() * 12) + 1);
-    iframe.width = "100%"
-    iframe.height = "100";
-    iframe.id = "hideframe"
-    // iframe.src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/331768602%3Fsecret_token%3Ds-d27yO&amp;color=ff5500&amp;auto_play=true&amp;start_track=" + start_track + "&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true;"
-    iframe.src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/339737358%3Fsecret_token%3Ds-AQv2I&amp;color=ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"
-    document.body.appendChild(iframe);
+    if (!isTouchDevice()) {
+        // load soundcloud player
+        var iframe = document.createElement('iframe');
+        var start_track = Math.floor((Math.random() * 12) + 1);
+        iframe.width = "100%"
+        iframe.height = "100";
+        iframe.id = "hideframe"
+        // iframe.src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/331768602%3Fsecret_token%3Ds-d27yO&amp;color=ff5500&amp;auto_play=true&amp;start_track=" + start_track + "&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true;"
+        iframe.src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/339737358%3Fsecret_token%3Ds-AQv2I&amp;color=ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"
+        document.body.appendChild(iframe);
+    } else {
+        alert('touch device')
+    }
+    // Load font
+    font = loadFont('assets/BodoniFLF-Italic.ttf');
 
+    // Load image
+    background_img = loadImage("assets/background_image.jpg");
+
+}
+
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+
+    // Set window title
+    document.title = " D E E P   C A R E ";
+
+    // LOGO_silver
     // Add logo to top-left corner
     var logo;
     logo = document.createElement('img');
@@ -37,36 +55,28 @@ function preload() {
         window.open('https://anyines.bandcamp.com');
     };
     logo.src = "assets/LOGO_silver.png";
-    // logo.src = "assets/anyines_logo.svg";
     document.body.appendChild(logo);
-
-
-    // Load font
-    font = loadFont('assets/BodoniFLF-Italic.ttf');
-}
-
-var background_img;
-
-function setup() {
-    createCanvas(window.innerWidth - borderPadding, window.innerHeight - borderPadding);
-
-    background_img = loadImage("assets/background_image.png");
-    image(background_img, width, height);
-
-    // Set window title
-    document.title = " D E E P   C A R E ";
 
     // Init text position
     textPosX = width / 2;
     textPosY = height / 2;
     // specify multiple formats for different browsers
-    backgroundVideo = createVideo(['assets/videos/IMG_1156.mp4' /*, 'assets/video_bg.mov'*/ ]);
+    backgroundVideo = createVideo(['assets/videos/IMG_320x240.mp4' /*, 'assets/video_bg.mov'*/ ], videoLoaded);
     backgroundVideo.hide();
     backgroundVideo.loop();
 }
 
+function videoLoaded(){
+  isVideoLoaded = true;
+}
+
 function draw() {
-    image(backgroundVideo, 0, 0, width, height); // draw the video frame to canvas
+    if(isVideoLoaded){
+      image(backgroundVideo, 0, 0, width, height); // draw the video frame to canvas
+    }else{
+      fill(255);
+      image(background_img, 0, 0, width, height);
+    }
 
     // Draw text
     fill(255, opacity);
@@ -110,5 +120,22 @@ function updateText() {
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth - borderPadding, windowHeight - borderPadding);
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function mouseReleased() {
+    window.open('https://anyines.bandcamp.com');
+}
+
+function deviceShaken() {
+    window.open('https://anyines.bandcamp.com');
+}
+
+function isTouchDevice() {
+    try {
+        document.createEvent("TouchEvent");
+        return true;
+    } catch (e) {
+        return false;
+    }
 }
